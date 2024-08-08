9e5e0b5be4a2690f0abe7fd5ab9564ad2f3551ce
Ashad Malik Kotla <ashadmalikkotla@gmail.com>
2024-08-08 15:30:03 +0500

    added game card skeletons when fetching games

---
+++ src/components/GameCardSkeleton.tsx
@@ -0,0 +1,19 @@
+import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
+
+const GameCardSkeleton = () => {
+  return (
+    <Card>
+      <Skeleton
+        width="300px"
+        height="200px"
+        borderRadius={10}
+        overflow={"hidden"}
+      />
+      <CardBody>
+        <SkeletonText />
+      </CardBody>
+    </Card>
+  );
+};
+
+export default GameCardSkeleton;
